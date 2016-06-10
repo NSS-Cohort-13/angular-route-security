a49ce38474ab43069f11501f6cfa2a5a3e5cc1b9
Scott Humphries <git@sscotth.io>
2016-06-10 13:47:45 -0500

    several private routes

--- public/main.js
+++ public/main.js
@@ -2,10 +2,26 @@ angular.module('app', ['ngRoute'])
   .config(($routeProvider) => {
     $routeProvider
       .when('/', {
-        template: '<h1>Public Page</h1><a href="#/private">Go To Private</a>',
+        template: `<h1>Public Page</h1>
+          <a href="#/private1">Go To Private 1</a>
+          <a href="#/private2">Go To Private 2</a>
+          <a href="#/private3">Go To Private 3</a>
+        `,
       })
-      .when('/private', {
-        template: '<h1>Private Page</h1><a href="#/">Go To Public</a>',
+      .when('/private1', {
+        template: '<h1>Private Page 1</h1><a href="#/">Go To Public</a>',
+        resolve: [
+          (authFactory, $location) => authFactory.isLoggedIn || $location.path('/'),
+        ],
+      })
+      .when('/private2', {
+        template: '<h1>Private Page 2</h1><a href="#/">Go To Public</a>',
+        resolve: [
+          (authFactory, $location) => authFactory.isLoggedIn || $location.path('/'),
+        ],
+      })
+      .when('/private3', {
+        template: '<h1>Private Page 3</h1><a href="#/">Go To Public</a>',
         resolve: [
           (authFactory, $location) => authFactory.isLoggedIn || $location.path('/'),
         ],
